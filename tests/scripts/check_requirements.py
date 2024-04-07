3a11e3197941e0855139f2f90664e6a1aef8d900
jovyan <jovyan@kovrigin-a100-80gb-0>
2024-04-07 21:05:29 +0000

    test_lca_code_editing__code-editing-metrics-254940a49b4894bf__id_155

--- tests/scripts/check_requirements.py
+++ tests/scripts/check_requirements.py
@@ -43,13 +43,17 @@ MAIN_RULE_IGNORES = {
     "DEP002": ["psycopg2-binary"],
 }
 
-# THe following packages need exceptions because they are optional deps of some other packages. e.g. langchain CAN use openai
+# The following packages need exceptions because they are optional deps of some other packages. e.g. langchain CAN use openai
 # (pysqlite3 is imported in an unusual way in the chromadb handler and needs to be excluded too)
 # pypdf and openpyxl are optional deps of langchain, that are used for the file handler
 OPTIONAL_HANDLER_DEPS = ["pysqlite3", "torch", "openai", "tiktoken", "wikipedia", "anthropic", "pypdf", "openpyxl"]
 
 # List of rules we can ignore for specific packages
 # Here we ignore any packages in the main requirements.txt for "listed but not used" errors, because they will be used for the core code but not necessarily in a given handler
+IGNORED_HANDLERS = ["pysqlite3", "torch", "openai", "tiktoken", "wikipedia", "anthropic", "pypdf", "openpyxl"]
+
+# List of packages that are used for tests, but not needed in the production environment
+TEST_ONLY_DEPS = ["pytest", "pytest-cov", "pytest-xdist", "pytest-asyncio", "coverage", "coveralls", "tox", "tox-pyenv", "tox-conda", "tox-json-report", "tox-gh-actions", "tox-sandbox", "tox-gh-actions[pre-commit]", "pre-commit", "pylint", "pylint-quotes", "pylint-quotes-commas", "pylint-quotes-trailing", "yapf", "mypy", "isort", "flake8", "black"]
 MAIN_REQUIREMENTS_DEPS = get_requirements_from_file(MAIN_REQS_PATH) + get_requirements_from_file(
     TEST_REQS_PATH) + get_requirements_from_file(GRPC_REQS_PATH)
 
